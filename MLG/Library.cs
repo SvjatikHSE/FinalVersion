eb19d37af302623c21d57eacab48135b8ab2032f
Мирзоян Шагане Багратовна <shbmirzoyan@edu.hse.ru>
2018-06-05 22:27:02 +0300

    Added tranparent UI (just a template), Package class and some props in Question class

--- MLG/Library.cs
+++ MLG/Library.cs
@@ -8,6 +8,14 @@ namespace MLG
 {
     public class Question
     {
+        public int Id { get; set; }
         public string Task { get; set; }
+        public string Answer { get; set; }
+    }
+    public class Package
+    {
+        public int Id { get; set; }
+        public List<Question> Questions { get; set; }
+        public String Name { get; set; }
     }
 }
