b3656b430077d695a91c35353b41c3269f06f2da
Svjatoslav <gggggg222333444@mail.ru>
2018-06-12 19:56:36 +0300

    Shorp file path added

--- MLG/Migrations/Configuration.cs
+++ MLG/Migrations/Configuration.cs
@@ -3,7 +3,9 @@ namespace MLG.Migrations
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.IO;
     using System.Linq;
+    using System.Reflection;
 
     internal sealed class Configuration : DbMigrationsConfiguration<MLG.BDContext>
     {
@@ -13,14 +15,26 @@ namespace MLG.Migrations
             ContextKey = "MLG.BDContext";
         }
 
+        private string MapPath(string seedFile)
+        {
+
+            var absolutePath = new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath; //was AbsolutePath but didn't work with spaces according to comments
+            var directoryName = Path.GetDirectoryName(absolutePath);
+            var path = Path.Combine(directoryName,seedFile.TrimStart('~').Replace('/', '\\'));
+            path = path.Replace("MLG\\MLG", "MLG\\Game_UI");
+
+            return path;
+        }
+
         protected override void Seed(MLG.BDContext context)
         {
+
             Package package = new Package();
-            package.LoadData("C:/Users/User/source/repos/MLG/Game_UI/TestFile.xml");
+            package.LoadData(MapPath("TestFile.xml"));
             Package package2 = new Package();
-            package2.LoadData("C:/Users/User/source/repos/MLG/Game_UI/TestFile2.xml");
+            package2.LoadData(MapPath("TestFile2.xml"));
             Package package3 = new Package();
-            package3.LoadData("C:/Users/User/source/repos/MLG/Game_UI/TestFile3.xml");
+            package3.LoadData(MapPath("TestFile3.xml"));
             using (var dbContext = new BDContext())
             {
                 dbContext.Packages.AddOrUpdate(x=>x.Name,package);
